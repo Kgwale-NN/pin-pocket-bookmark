049c602073bd6925598e902ce9e738f9e95651ea
Kgwale NN <kgoalemothiba8@gmail.com>
2026-09-23 16:20:35 +0200

    Remove unused footer imports

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,19 +2,6 @@ import React from 'react'
 import styles from './Footer.module.css'
 import {Text} from '../text/Text'
 
-import Logo from '../../assets/logo.png'
-import Github from '../../assets/github.png'
-import Instagram from '../../assets/instagram.png'
-import Facebook from '../../assets/facebook.png'
-import  Twitter from '../../assets/x.png'
-import Pinterest from '../../assets/pinterest.png'
-import Linkedin from '../../assets/linkedin.png'
-
-
-
-
-
-
 export const Footer = () => {
   return (
 
